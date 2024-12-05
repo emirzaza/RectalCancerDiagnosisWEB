14eb609a194223c7208f904979a6def3d67de1b0
Emir Zaza <e.yasarzaza@gmail.com>
2024-12-05 22:57:02 +0300

    ADD _Layout , UPDATE index page

---
+++ RectalCancerDiagnosisWeb/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RectalCancerDiagnosisWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        public IActionResult signup()
+        {
+            return View();
+        }
+    }
+}
