7e8ab61e9cab2db53e0d8c7352215f0c79517ced
Emir Zaza <e.yasarzaza@gmail.com>
2024-12-06 21:01:01 +0300

    ADD sign up

--- RectalCancerDiagnosisWeb/Controllers/AccountController.cs
+++ RectalCancerDiagnosisWeb/Controllers/AccountController.cs
@@ -1,17 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RectalCancerDiagnosisWeb.Models;
 
 namespace RectalCancerDiagnosisWeb.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
         public IActionResult login()
         {
             return View();
         }
-
+        [HttpGet]
         public IActionResult signup()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult signup(User newuser)
+        {
+           _context.Users.Add(newuser);
+           _context.SaveChanges();
+
+            return RedirectToAction("login");
         }
     }
 }
